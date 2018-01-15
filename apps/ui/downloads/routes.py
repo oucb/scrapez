ca0c942869dd42665650c672fcbf13c1fc9dbe8b
Olivier Cervello <olivier.cervello@gmail.com>
2018-01-15 15:04:38 -0600

    Update downloads

--- apps/ui/downloads/routes.py
+++ apps/ui/downloads/routes.py
@@ -2,6 +2,19 @@ from . import downloads
 from flask import send_from_directory, current_app as app, url_for, jsonify
 import os
 
+EXTENSIONS_WHITELIST = [
+    'mp4',
+    'avi',
+    'pdf'
+]
+EXTENSIONS_BLACKLIST = [
+    'exe',
+    'sh',
+    'o',
+    'dll',
+    'torrent'
+]
+
 @downloads.route('/<path:filename>')
 def send_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename)
@@ -9,13 +22,13 @@ def send_file(filename):
 @downloads.route('/list')
 def list_downloads():
     items = []
-    for file in os.listdir(app.config['DOWNLOAD_FOLDER']):
-        ext = file.split('.')[-1]
-        if ext in ['mp4', 'avi']:
-            path = os.path.join(app.config['DOWNLOAD_FOLDER'], file)
-            download_url = url_for('downloads.send_file', filename=path)
+    for filename in os.listdir(app.config['DOWNLOAD_FOLDER']):
+        extension = filename.split('.')[-1]
+        if extension in ['mp4', 'avi']:
+            path = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
+            download_url = url_for('downloads.send_file', filename=filename)
             item = {
-                'path': path,
+                'filename': filename,
                 'download_url': download_url
             }
             items.append(item)

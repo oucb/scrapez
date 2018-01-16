3bdfd09c35436354358de9c87d74f6ee6eb6f864
Olivier Cervello <olivier.cervello@gmail.com>
2018-01-15 23:22:32 -0600

    Add SSL support

--- apps/ui/extensions.py
+++ apps/ui/extensions.py
@@ -1,15 +1,19 @@
 from flask_socketio import SocketIO
 from flask_jsglue import JSGlue
 from flask_caching import Cache
+from flask_sslify import SSLify
 import eventlet
 eventlet.monkey_patch(socket=True)
 
 socketio = SocketIO()
 jsglue = JSGlue()
 cache = Cache()
+sslify = SSLify()
+
 
 EXTENSIONS = [
     (cache, {'config': {'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://localhost'}}),
     (socketio, {'message_queue': 'redis://localhost', 'async_mode': 'eventlet'}),
-    jsglue
+    jsglue,
+    sslify
 ]

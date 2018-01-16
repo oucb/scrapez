1b15b72e94570bcb48cf1641e787d49bd25d2c9e
Olivier Cervello <olivier.cervello@gmail.com>
2018-01-16 01:58:18 -0600

    Update chat

--- apps/static/js/chat_events.js
+++ apps/static/js/chat_events.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
       var new_name = data.new_name;
       var msg = "<'" + data.old_name + "' changed his name to '" + data.new_name + "'>"
       console.log(msg)
-      emit('message', {msg: msg})
+      write_chat('#chat', msg)
     })
     $('#text').keypress(function(e){
         console.log("Text event received.")
